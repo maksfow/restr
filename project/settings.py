2d8502427ed433eb63377437dfd5f5d71c5cf14e
maksfow <maksfow@gmail.com>
2024-02-10 20:13:58 +0500

    nn

--- project/settings.py
+++ project/settings.py
@@ -118,7 +118,7 @@ USE_TZ = True
 import os
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [
- os.path.join(BASE_DIR / "static"),
+ os.path.join(BASE_DIR, "static"),
 ]
 
 # Default primary key field type
